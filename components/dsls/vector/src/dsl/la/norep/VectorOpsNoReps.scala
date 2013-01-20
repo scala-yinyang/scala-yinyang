8639be870511522cd923874fa9b15e147655a28c
vova <vova@vova-N82JV.(none)>
2013-01-21 01:38:56 +0400

    VectorOpsNorReps dsl merged

--- components/dsls/vector/src/dsl/la/norep/VectorOpsNoReps.scala
+++ components/dsls/vector/src/dsl/la/norep/VectorOpsNoReps.scala
@@ -6,11 +6,19 @@ import base._
 trait Base extends LiftBase
 
 trait IntDSL extends Base {
+  self: DoubleDSL ⇒
+
   type Int = IntOps
 
+  //TODO (TOASK) where do we provide the implementation for this methods (in result DSL object)
   trait IntOps {
     def +(that: Int): Int
-    // TODO complete
+    def +(that: Double): Double
+    def *(that: Int): Int
+    def *(that: Double): Double
+    def unary_- : Int
+    def toInt: Int
+    def toDouble: Double
   }
 
   implicit object LiftInt extends LiftEvidence[scala.Int, Int] {
@@ -20,6 +28,41 @@ trait IntDSL extends Base {
   implicit object LiftUnit extends LiftEvidence[scala.Unit, Unit] {
     def lift(v: Unit): Unit = ???
   }
+
+  implicit def intOpsToDoubleOps(conv: Int): Double = ???
+}
+
+trait DoubleDSL extends Base {
+  selfType: IntDSL ⇒
+
+  type Double = DoubleOps
+
+  trait DoubleOps {
+    def +(that: Double): Double
+    def +(that: Int): Double
+
+    def *(that: Int): Double
+    def *(x: Double): Double
+    def toInt: Int
+    def toDouble: Double
+    def unary_- : Double
+
+    //TODO this defs not from original Double and just to provide
+    //implementation for Vector operations
+    def pow(power: Double)
+    def sqrt
+  }
+
+  //TODO (TOASK) maybe extends LiftEvidence[scala.Double, DoubleDSL#Double]
+  implicit object LiftDouble extends LiftEvidence[scala.Double, Double] {
+    def lift(v: scala.Double): Double = ???
+  }
+
+  //TODO (TOASK) we can remove it implicit object from here because we have
+  //selfType IntDSL
+  //  implicit object LiftUnit extends LiftEvidence[scala.Unit, Unit] {
+  //    def lift(v: Unit): Unit = ???
+  //  }
 }
 
 trait ClassTagOps extends Base {
@@ -34,7 +77,10 @@ trait ClassTagOps extends Base {
   val ClassTag = scala.reflect.ClassTag
 }
 
-trait NumericOps extends IntDSL with Base {
+//TODO (TOASK) check the correctness of this implementation (correctness for DSL usage)
+//maybe implement Ordering to get it complexier
+//maybe rename to NumericLifted
+trait NumericOps extends IntDSL with DoubleDSL with Base {
   type Numeric[T] = NumericOps[T]
 
   trait NumericOps[T] {
@@ -44,28 +90,63 @@ trait NumericOps extends IntDSL with Base {
     def negate(x: T): T
     def fromInt(x: Int): T
     def toInt(x: T): Int
-    def toLong(x: T): Long
-    def toFloat(x: T): Float
     def toDouble(x: T): Double
-    // TODO complete
+
+    def zero = fromInt(liftTerm(0))
+    def one = fromInt(liftTerm(1))
+
+    def abs(x: T): T = ???
+    def signum(x: T): Int = ???
+
+    class Ops(lhs: T) {
+      def +(rhs: T) = plus(lhs, rhs)
+      def -(rhs: T) = minus(lhs, rhs)
+      def *(rhs: T) = times(lhs, rhs)
+      def unary_-() = negate(lhs)
+
+      def abs(): T = NumericOps.this.abs(lhs)
+      def toInt(): Int = NumericOps.this.toInt(lhs)
+      def toDouble(): Double = NumericOps.this.toDouble(lhs)
+    }
+
+    implicit def mkNumericOps(lhs: T): Ops = new Ops(lhs)
   }
+
+  //companion for trait Numeric Ops
+  //here we can provide all implicit objects
+  //TODO try to get it working inside object NumericOps
+  //  object NumericOps {
 
   implicit object IntIsIntegral extends Numeric[Int] {
-    def fromInt(x: IntOps): IntOps = ???
-    def minus(x: IntOps, y: IntOps): IntOps = ???
-    def negate(x: IntOps): IntOps = ???
-    def plus(x: IntOps, y: IntOps): IntOps = ???
-    def times(x: IntOps, y: IntOps): IntOps = ???
-    // TODO these need to return the lifted types. This means that Numeric Type needs to be changed to something else.
-    def toDouble(x: IntOps): Double = ???
-    def toFloat(x: IntOps): Float = ???
-    def toInt(x: IntOps): IntOps = ???
-    def toLong(x: IntOps): scala.Long = ???
-    def compare(x: IntOps, y: IntOps): IntOps = ???
+    def plus(x: Int, y: Int): Int = ???
+    def minus(x: Int, y: Int): Int = ???
+    def times(x: Int, y: Int): Int = ???
+    def negate(x: Int): Int = ???
+
+    def fromInt(x: Int): Int = ???
+    def toInt(x: Int): Int = ???
+    def toDouble(x: Int): Double = ???
   }
+
+  implicit object DoubleIsIntegral extends Numeric[Double] {
+    def plus(x: Double, y: Double): Double = ???
+    def minus(x: Double, y: Double): Double = ???
+    def times(x: Double, y: Double): Double = ???
+    def negate(x: Double): Double = ???
+    def fromInt(x: Int): Double = ???
+    // TODO these need to return the lifted types. This means that Numeric Type needs to be changed to something else.
+    def toInt(x: Double): Int = ???
+    def toDouble(x: Double): Double = ???
+  }
+  //  }
+
+  //TODO find another place
+  //TODO (TOASK) problem how to write Double type or where to place this implicit?
+  //TODO if it works correctly in IntDSL and check DEPENDENCIES (correctness mixing of DoubleDSL in IntDSL
+  //  implicit def intOpsToDoubleOps(conv: Int): Double = ???
 }
 
-trait ArrayDSL extends Base {
+trait ArrayDSL extends Base with IntDSL with DoubleDSL {
   type Array[T] = ArrayOps[T]
 
   trait ArrayOps[T] {
@@ -75,6 +156,7 @@ trait ArrayDSL extends Base {
 
   object Array {
     def apply[T](values: T*): Array[T] = ???
+    def fill[T: ClassTag](n: Int)(elem: ⇒ T): Array[T] = ???
     // TODO complete
   }
 
@@ -92,17 +174,64 @@ trait IfThenElseDSL extends BooleanDSL with Base {
   def __ifThenElse[T](c: ⇒ Boolean, t: T, e: T) = ???
 }
 
-trait VectorDSL extends ClassTagOps with IfThenElseDSL with ArrayDSL with IntDSL with NumericOps with Base with Interpret {
+//TODO try to remove it and test without TupleDSL
+trait TupleDSL extends Base {
+  type Tuple2[T1, T2] = Tuple2Ops[T1, T2]
+
+  trait Tuple2Ops[T1, T2] extends AnyRef {
+    def _1: T1
+    def _2: T2
+    def swap: Tuple2[T2, T1]
+
+    // !!!! TODO (TOASK) find place for code lifting (do we need this lifting)
+    implicit object LiftTuple2 extends LiftEvidence[scala.Tuple2[T1, T2], Tuple2[T1, T2]] {
+      def lift(v: scala.Tuple2[T1, T2]): Tuple2[T1, T2] = ???
+    }
+  }
+
+  object Tuple2 {
+    def apply[T1, T2](x1: T1, x2: T2): Tuple2[T1, T2] = ???
+
+  }
+}
+
+trait VectorDSL extends ClassTagOps with IfThenElseDSL with ArrayDSL with TupleDSL with IntDSL with DoubleDSL with NumericOps with Base with Interpret {
   type Vector[T] = VectorOps[T]
 
+  //TODO test and correct its usage
+  //  trait VectorTransformer[T] {
+  //    def transform(v: Vector[T]): Vector[T]
+  //  }
+
   trait VectorOps[T] {
+    //TODO test and correct its usage
+    //  implicit object VectorFunction extends VectorTransformer[T] {
+    //    def transform(v: Vector[T]): Vector[T] = ???
+    //  }
     def *(v: Vector[T]): Vector[T]
     def +(v: Vector[T]): Vector[T]
     def map[U: Numeric: ClassTag](v: T ⇒ U): Vector[U]
+
+    //TODO it's a bad idea to provide here implementation
+    def baseVectors: Array[Vector[T]] = ??? //find base vectors
+
+    def partition(fun: T ⇒ Boolean): Tuple2[Vector[T], Vector[T]]
+
+    def dotProduct(v: Vector[T]): T
+
+    def splice(vs: Vector[T]*): Vector[T]
+
+    def spliceT(v: Tuple2[Vector[T], Vector[T]]): Vector[T]
+
+    //    def transform(tr: VectorTransformer[T]): Vector[T]
   }
 
   object DenseVector {
     def apply[T: Numeric: ClassTag](a: T*): Vector[T] = ???
+
+    //    def apply(a: Double*): Vector[Double] = ???
+    //    def apply[T <: Double,  Double: Numeric: ClassTag](a: T*): Vector[T] = ???
+    //    def apply[T <: AnyVal: Numeric: ClassTag](a: Map[Int, T]): Vector[T] = ???
   }
 
   /**
@@ -110,6 +239,9 @@ trait VectorDSL extends ClassTagOps with IfThenElseDSL with ArrayDSL with IntDSL
    */
   object SparseVector {
     def apply[T: Numeric: ClassTag](a: T*): Vector[T] = ???
+
+    //TODO required to model Map to use
+    //    def apply[T: Numeric: ClassTag](a: Map[Int, T]): Vector[T] = ???
   }
 
   object TestObject {
