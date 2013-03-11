d6e3dc61940bef5e0bf8884e3024269e201907c5
Vojin Jovanovic <vojin.jovanovic@epfl.ch>
2013-03-11 14:42:06 +0100

    Custom Scala

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,7 @@ object MPDEBuild extends Build {
   lazy val scalaOrg = "org.scala-lang"
   lazy val defaults = Defaults.defaultSettings ++ formatSettings ++ Seq(
     // scala version + resolver
-    //scalaHome := Some(file(Path.userHome + "/scala/build/pack"))
+    scalaHome := Some(file(Path.userHome + "/work/devl/scalac/myscala/build/pack")),
     scalaOrganization := scalaOrg,
     scalaVersion := "2.10.1-SNAPSHOT",
     resolvers in ThisBuild += ScalaToolsSnapshots,
