5562a5af3156ac7ce88c4f53b6f226987f7247c5
Amir Shaikhha <amir.shaikhha@typesafe.com>
2013-07-02 19:48:20 +0200

    HoleTypeAnalyser and liftTypes config added, hole method uses parameter index.
    * A marker trait FullyUnstaged added, which specifies that all captured variables should be hole.
    * A marker trait HoleTypeAnalyser added, which specifies that the captured variables which their type is passed to configuration as liftTypes should be lifted an other captured variables should be substituted by hole.
    * HoleTransformation changed to include index of parameter instead of symbolId. So the variables with the same symbolId would have the same parameter index. So in the HoleTransformation we would create a holeTable which is mapping from symbolId to index. When we want to generate `interpret` invokation, the parameters would be ordered by their index to have a correct binding for them.

--- components/framework/src/ch/epfl/yinyang/api/FullyUnstaged.scala
+++ components/framework/src/ch/epfl/yinyang/api/FullyUnstaged.scala
@@ -3,11 +3,11 @@ package ch.epfl.yinyang.api
 import reflect.runtime.universe.Symbol
 
 /**
- * Marker trait for DSLs in which all holes are treated as constants.
+ * Marker trait for DSLs in which no hole is treated as constant and all holes are needed to be recomputed.
  *
  * NOTE: DSLs that inherit this trait will not be reflectively instantiated at compile time.
  */
-trait FullyStaged { this: BaseYinYang =>
+trait FullyUnstaged { this: BaseYinYang =>
 
   override def requiredHoles(symbols: List[Symbol]): List[Int] =
     throw new RuntimeException("This method must not be called!!!")
--- components/framework/src/ch/epfl/yinyang/api/HoleTypeAnalyser.scala
+++ components/framework/src/ch/epfl/yinyang/api/HoleTypeAnalyser.scala
@@ -1,14 +1,14 @@
+
 package ch.epfl.yinyang.api
 
 import reflect.runtime.universe.Symbol
 
 /**
- * Marker trait for DSLs in which all holes are treated as constants.
+ * Marker trait for DSLs in which identifying holes are done by lifted type information.
  *
  * NOTE: DSLs that inherit this trait will not be reflectively instantiated at compile time.
  */
-trait FullyStaged { this: BaseYinYang =>
-
+trait HoleTypeAnalyser { this: BaseYinYang =>
   override def requiredHoles(symbols: List[Symbol]): List[Int] =
     throw new RuntimeException("This method must not be called!!!")
 
