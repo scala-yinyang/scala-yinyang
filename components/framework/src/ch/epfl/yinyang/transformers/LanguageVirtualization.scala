26f3fea38c25943db3a05b6c31e43a455b1c37d9
Sandro Stucki <sandro.stucki@gmail.com>
2013-08-12 15:38:21 +0200

    Removes redundant double-transformations.

--- components/framework/src/ch/epfl/yinyang/transformers/LanguageVirtualization.scala
+++ components/framework/src/ch/epfl/yinyang/transformers/LanguageVirtualization.scala
@@ -12,6 +12,7 @@ import scala.collection.mutable
  * arbitrary semantics.
  *
  * Features covered are:
+ *   var x = e          =>       var x = __newVar(e)
  *   if(c) t else e     =>       __ifThenElse(c, t, e)
  *   return t           =>       __return(t)
  *   x = t              =>       __assign(x, t)
@@ -50,73 +51,81 @@ trait LanguageVirtualization extends MacroModule with TransformationUtils with D
 
   private class VirtualizationTransformer extends Transformer {
     val lifted = mutable.ArrayBuffer[DSLFeature]()
-    def liftFeature(receiver: Option[Tree], nme: String, args: List[List[Tree]], targs: List[Tree] = Nil): Tree = {
-      lifted += DSLFeature(receiver.map(_.tpe), nme, targs, args.map(_.map(_.tpe)))
-      log(show(method(receiver.map(transform), nme, args.map(_.map(transform)), targs)), 3)
-      method(receiver.map(transform), nme, args.map(_.map(transform)), targs)
+
+    def liftFeature(receiver: Option[Tree], nme: String, args: List[Tree], targs: List[Tree] = Nil): Tree = {
+      lifted += DSLFeature(receiver.map(_.tpe), nme, targs, List(args.map(_.tpe)))
+      log(show(method(receiver.map(transform), nme, List(args.map(transform)), targs)), 3)
+      method(receiver.map(transform), nme, List(args.map(transform)), targs)
     }
+
     override def transform(tree: Tree): Tree = {
       tree match {
+        // sstucki: It seems necessary to keep the MUTABLE flag in the
+        // new ValDef set, otherwise it becomes tricky to
+        // "un-virtualize" a variable definition, if necessary
+        // (e.g. if the DSL does not handle variable definitions in a
+        // special way).
         case ValDef(mods, sym, tpt, rhs) if mods.hasFlag(Flag.MUTABLE) =>
-          ValDef(mods, sym, tpt, liftFeature(None, "__newVar", List(List(rhs))))
+          ValDef(mods, sym, tpt, liftFeature(None, "__newVar", List(rhs)))
 
         case t @ If(cond, thenBr, elseBr) =>
-          liftFeature(None, "__ifThenElse", List(List(transform(cond), transform(thenBr), transform(elseBr))))
+          liftFeature(None, "__ifThenElse", List(cond, thenBr, elseBr))
 
         case Return(e) =>
-          liftFeature(None, "__return", List(List(transform(e))))
+          liftFeature(None, "__return", List(e))
 
         case Assign(lhs, rhs) =>
-          liftFeature(None, "__assign", List(List(transform(lhs), transform(rhs))))
+          liftFeature(None, "__assign", List(lhs, rhs))
 
         case LabelDef(sym, List(), If(cond, Block(body :: Nil, Apply(Ident(label),
           List())), Literal(Constant()))) if label == sym => // while(){}
-          liftFeature(None, "__whileDo", List(List(transform(cond), transform(body))))
+          liftFeature(None, "__whileDo", List(cond, body))
 
         case LabelDef(sym, List(), Block(body :: Nil, If(cond, Apply(Ident(label),
           List()), Literal(Constant())))) if label == sym => // do while(){}
-          liftFeature(None, "__doWhile", List(List(transform(cond), transform(body))))
+          liftFeature(None, "__doWhile", List(cond, body))
 
         case Apply(Select(qualifier, TermName("$eq$eq")), List(arg)) =>
-          liftFeature(Some(transform(qualifier)), "__$eq$eq", List(List(transform(arg))))
+          liftFeature(Some(qualifier), "__$eq$eq", List(arg))
 
         case Apply(Select(qualifier, TermName("$bang$eq")), List(arg)) =>
-          liftFeature(Some(transform(qualifier)), "__$bang$eq", List(List(transform(arg))))
+          liftFeature(Some(qualifier), "__$bang$eq", List(arg))
 
         case Apply(lhs @ Select(qualifier, TermName("eq")), List(arg)) =>
-          liftFeature(Some(transform(qualifier)), "__eq", List(List(transform(arg))))
+          liftFeature(Some(qualifier), "__eq", List(arg))
 
+        // sstucki: What is this?
         case Apply(lhs @ Select(qualifier, TermName("ne")), List(arg)) =>
-          liftFeature(Some(transform(qualifier)), "__ne", List(List(transform(arg))))
+          liftFeature(Some(qualifier), "__ne", List(arg))
 
         case Apply(lhs @ Select(qualifier, TermName("hashCode")), List()) =>
-          liftFeature(Some(transform(qualifier)), "__hashCode", List(List()))
+          liftFeature(Some(qualifier), "__hashCode", List())
 
         case Apply(lhs @ Select(qualifier, TermName("$hash$hash")), List()) =>
-          liftFeature(Some(transform(qualifier)), "__$hash$hash", List(List()))
+          liftFeature(Some(qualifier), "__$hash$hash", List())
 
         case TypeApply(Select(qualifier, TermName("asInstanceOf")), targs) =>
-          liftFeature(Some(transform(qualifier)), "__asInstanceOf", List(List()), targs)
+          liftFeature(Some(qualifier), "__asInstanceOf", List(), targs)
 
         case TypeApply(Select(qualifier, TermName("isInstanceOf")), targs) =>
-          liftFeature(Some(transform(qualifier)), "__isInstanceOf", List(List()), targs)
+          liftFeature(Some(qualifier), "__isInstanceOf", List(), targs)
 
         case Apply(Select(qualifier, TermName("notify")), List()) =>
-          liftFeature(Some(transform(qualifier)), "__notify", List(List()))
+          liftFeature(Some(qualifier), "__notify", List())
 
         case Apply(Select(qualifier, TermName("notifyAll")), List()) =>
-          liftFeature(Some(transform(qualifier)), "__notifyAll", List())
+          liftFeature(Some(qualifier), "__notifyAll", List())
 
         case Apply(Select(qualifier, TermName("wait")), List()) =>
-          liftFeature(Some(transform(qualifier)), "__wait", List())
+          liftFeature(Some(qualifier), "__wait", List())
 
         case Apply(Select(qualifier, TermName("wait")), List(arg)
           ) if arg.tpe =:= typeOf[Long] =>
-          liftFeature(Some(transform(qualifier)), "__wait", List(List(arg)))
+          liftFeature(Some(qualifier), "__wait", List(arg))
 
         case Apply(Select(qualifier, TermName("wait")), List(arg0, arg1)
           ) if arg0.tpe =:= typeOf[Long] && arg1.tpe =:= typeOf[Int] =>
-          liftFeature(Some(transform(qualifier)), "__wait", List(List(arg0, arg1)))
+          liftFeature(Some(qualifier), "__wait", List(arg0, arg1))
 
         case _ =>
           super.transform(tree)
