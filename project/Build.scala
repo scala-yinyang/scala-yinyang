fab732e7f4fb294990d968ab91ed98a6e9869138
Vojin Jovanovic <gvojin@gmail.com>
2014-01-31 14:26:37 +0100

    Ready for website and publishing.

--- project/Build.scala
+++ project/Build.scala
@@ -1,14 +1,107 @@
 import java.io.File
 import sbt._
 import Keys._
+import com.typesafe.sbt.SbtSite.SiteKeys._
 import Process._
 import com.typesafe.sbt.SbtScalariform
 import com.typesafe.sbt.SbtScalariform.ScalariformKeys
+import com.typesafe.sbt.SbtSite.site
+import com.typesafe.sbt.site.JekyllSupport.Jekyll
+import com.typesafe.sbt.SbtGhPages.ghpages
+import com.typesafe.sbt.SbtGit.git
 
 object YinYangBuild extends Build {
   lazy val projectSettings = Seq[Setting[_]](
-    version      := "0.1-SNAPSHOT", 
-    organization := "ch.epfl.lamp"
+    version              := "0.1-SNAPSHOT", 
+    organization         := "ch.epfl.lamp",
+    licenses             := Seq("New BSD" -> url("https://raw2.github.com/vjovanov/yin-yang/master/LICENSE")),
+    homepage             := Some(url("http://scala-yinyang.org/")),
+    organizationHomepage := Some(url("http://lamp.epfl.ch")),
+    scmInfo              := Some(ScmInfo(url("https://github.com/vjovanov/yin-yang.git"),"git://github.com/vjovanov/yin-yang.git"))
+  )
+
+  lazy val scalaSettings = Defaults.defaultSettings ++ Seq(
+    scalaOrganization    := scalaOrg,
+    scalaVersion         := "2.10.3",
+    scalacOptions        := defaultScalacOptions :+ "-Xfatal-warnings"
+  )
+
+  // libraries
+  lazy val libraryDeps = Seq(
+    libraryDependencies <<= scalaVersion(ver => Seq(
+      scalaOrg % "scala-library" % ver,
+      scalaOrg % "scala-reflect" % ver,
+      scalaOrg % "scala-compiler" % ver,
+      "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
+      "com.github.axel22" %% "scalameter" % "0.4",
+      "junit" % "junit" % "4.11" % "test" // we need JUnit explicitly
+  )))
+
+  // modules
+  lazy val _yinyang        = Project(id = "root",                      base = file("."), settings = Project.defaultSettings ++ Seq(publishArtifact := false)) aggregate (yinyang, yy_core, yy_paradise, vector_dsl, vector_dsl_test)
+  lazy val yy_core         = Project(id = "yy-core",                   base = file("components/core"), settings = defaults ++ Seq(name := "yy-core"))
+  lazy val yy_paradise     = Project(id = "yy-paradise",               base = file("components/paradise"), settings = defaults ++ paradise ++ Seq(name := "yy-paradise")) dependsOn(yy_core)
+  lazy val yinyang         = Project(id = "yin-yang",                  base = file("components/yin-yang"), settings = defaults ++ paradise ++ Seq(name := "yin-yang")) dependsOn(yy_core)
+  lazy val vector_dsl      = Project(id = "yy-vector-dsl",             base = file("components/dsls/vector"), settings = defaults) dependsOn(yinyang)
+  lazy val vector_dsl_test = Project(id = "yy-vector-dsl-test",        base = file("components/dsls/vector-test"), settings = defaults) dependsOn(yinyang, vector_dsl)
+
+  lazy val defaults = projectSettings ++ scalaSettings ++ formatSettings ++ libraryDeps ++ Seq(
+    resolvers +=  "OSSH" at "https://oss.sonatype.org/content/groups/public",
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+    testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
+    // paths - so we don't need to have src/main/scala ... just src/ test/ and resources/
+    scalaSource in Compile <<= baseDirectory(_ / "src"),
+    scalaSource in Test <<= baseDirectory(_ / "test"),
+    resourceDirectory in Compile <<= baseDirectory(_ / "resources"),
+    // sbteclipse needs some info on source directories:
+    unmanagedSourceDirectories in Compile <<= (scalaSource in Compile)(Seq(_)),
+    unmanagedSourceDirectories in Test <<= (scalaSource in Test)(Seq(_)),
+    parallelExecution in Test := false
+  )
+
+  // add the macro paradise compiler plugin
+  lazy val paradise = Seq(
+    addCompilerPlugin("org.scala-lang.plugins" % "macro-paradise_2.10.3" % "2.0.0-SNAPSHOT"),
+    scalacOptions := defaultScalacOptions
+  )
+
+
+  lazy val website = Seq(site.settings,
+    ghpages.settings,
+    site.includeScaladoc(),
+    site.jekyllSupport(),
+    git.remoteRepo := "git@github.com:vjovanov/yin-yang.git",
+    includeFilter in Jekyll := ("*.html" | "*.png" | "*.js" | "*.css" | "CNAME")
+  )
+
+  lazy val publishing = Seq(
+    // for integration testing against scala snapshots
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+   // so we don't have to wait for maven central synch
+   resolvers += Resolver.sonatypeRepo("releases"),
+   // publishing
+   credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+   // If we want on maven central, we need to be in maven style.
+   publishMavenStyle := true,
+   publishArtifact in Test := false,
+   // The Nexus repo we're publishing to.
+   publishTo := Some(
+     if (version.value.trim.endsWith("SNAPSHOT")) Resolver.sonatypeRepo("snapshots")
+     else Opts.resolver.sonatypeStaging
+   ),
+   // Maven central cannot allow other repos.  We're ok here because the artifacts we
+   // we use externally are *optional* dependencies.
+   pomIncludeRepository := { _ => false },
+   pomExtra := (
+     <developers>
+       <developer>
+         <id>vjovanov</id>
+         <name>Vojin Jovanovic</name>
+         <url>http://www.vjovanov.com/</url>
+       </developer>
+     </developers>
+   )
   )
 
   lazy val formatSettings = SbtScalariform.scalariformSettings ++ Seq(
@@ -25,54 +118,4 @@ object YinYangBuild extends Build {
   }
   lazy val defaultScalacOptions = Seq("-deprecation", "-feature", "-language:higherKinds", "-language:implicitConversions")
   lazy val scalaOrg = "org.scala-lang"
-  lazy val scalaSettings = Defaults.defaultSettings ++ Seq(
-    scalaOrganization := scalaOrg,
-    scalaVersion := "2.10.3",
-    scalacOptions := defaultScalacOptions :+ "-Xfatal-warnings"
-  )
-
-  lazy val defaults = projectSettings ++ scalaSettings ++ formatSettings ++ Seq(
-    resolvers +=  "OSSH" at "https://oss.sonatype.org/content/groups/public",
-    resolvers += Resolver.sonatypeRepo("snapshots"),
-    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-
-
-    testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
-
-    // paths - so we don't need to have src/main/scala ... just src/ test/ and resources/
-    scalaSource in Compile <<= baseDirectory(_ / "src"),
-    scalaSource in Test <<= baseDirectory(_ / "test"),
-    resourceDirectory in Compile <<= baseDirectory(_ / "resources"),
-
-    // sbteclipse needs some info on source directories:
-    unmanagedSourceDirectories in Compile <<= (scalaSource in Compile)(Seq(_)),
-    unmanagedSourceDirectories in Test <<= (scalaSource in Test)(Seq(_)),
-
-    // add the library, reflect and the compiler as libraries
-    libraryDependencies <<= scalaVersion(ver => Seq(
-      scalaOrg % "scala-library" % ver,
-      scalaOrg % "scala-reflect" % ver,
-      scalaOrg % "scala-compiler" % ver,
-      "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
-      "com.github.axel22" %% "scalameter" % "0.4",
-      "junit" % "junit" % "4.11" % "test" // we need JUnit explicitly
-    )),
-
-    // testing
-    parallelExecution in Test := false,
-    organization := "ch.epfl.lamp"
-  )
-
-  // add the macro paradise compiler plugin
-  lazy val paradise = Seq(
-    addCompilerPlugin("org.scala-lang.plugins" % "macro-paradise_2.10.3" % "2.0.0-SNAPSHOT"),
-    scalacOptions := defaultScalacOptions
-  )
-
-  lazy val _yinyang        = Project(id = "root",                      base = file("."), settings = Project.defaultSettings ++ Seq(publishArtifact := false)) aggregate (yinyang, yy_core, yy_paradise, vector_dsl, vector_dsl_test)
-  lazy val yy_core         = Project(id = "yy-core",                   base = file("components/core"), settings = defaults ++ Seq(name := "yy-core"))
-  lazy val yy_paradise     = Project(id = "yy-paradise",               base = file("components/paradise"), settings = defaults ++ paradise ++ Seq(name := "yy-paradise")) dependsOn(yy_core)
-  lazy val yinyang         = Project(id = "yin-yang",                  base = file("components/yin-yang"), settings = defaults ++ paradise ++ Seq(name := "yin-yang")) dependsOn(yy_core)
-  lazy val vector_dsl      = Project(id = "yy-vector-dsl",             base = file("components/dsls/vector"), settings = defaults) dependsOn(yinyang)
-  lazy val vector_dsl_test = Project(id = "yy-vector-dsl-test",        base = file("components/dsls/vector-test"), settings = defaults) dependsOn(yinyang, vector_dsl)
 }
--- project/plugin.sbt
+++ project/plugin.sbt
@@ -1,3 +1,8 @@
+resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven"
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.5.2")
+
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.1.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.1")
+
